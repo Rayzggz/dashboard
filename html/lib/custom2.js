217d67f547887c6f94108d1cadff65ddbae7c992
Roi Feng <37480123+Rayzggz@users.noreply.github.com>
2022-10-08 17:13:13 -0400

    page 1 done 2

---
+++ html/lib/custom2.js
@@ -0,0 +1,121 @@
+var graph_2_d1 = echarts.init(document.getElementById('graph_2_d1'));	
+window.onresize = function() {
+    graph_2_d1.resize();
+  };
+var option = {
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+graph_2_d1.setOption(option);
+
+
+
+var graph_2_d2 = echarts.init(document.getElementById('graph_2_d2'));	
+window.onresize = function() {
+    graph_2_d2.resize();
+  };
+var option = {
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+graph_2_d2.setOption(option);
+
+var graph_2_a2 = echarts.init(document.getElementById('graph_2_a2'));	
+window.onresize = function() {
+    graph_2_a2.resize();
+  };
+var option = {
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+graph_2_a2.setOption(option);
+
+
+var graph_2_b1 = echarts.init(document.getElementById('graph_2_b1'));	
+window.onresize = function() {
+    graph_2_b1.resize();
+  };
+var option = {
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+graph_2_b1.setOption(option);
+
+
+var graph_2_a1 = echarts.init(document.getElementById('graph_2_a1'));	
+window.onresize = function() {
+    graph_2_a1.resize();
+  };
+var option = {
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+graph_2_a1.setOption(option);
+
+
+
+var graph_2_b2 = echarts.init(document.getElementById('graph_2_b2'));	
+window.onresize = function() {
+    graph_2_b2.resize();
+  };
+var option = {
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+graph_2_b2.setOption(option);
+
+
