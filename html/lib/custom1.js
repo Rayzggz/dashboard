310812bf8cbc628491bbb1fda6f86f39053c74c1
Roi Feng <37480123+Rayzggz@users.noreply.github.com>
2022-10-08 22:58:58 -0400

    Update custom1.js

--- html/lib/custom1.js
+++ html/lib/custom1.js
@@ -2,49 +2,7 @@ document.getElementById('graph_1_d1').innerHTML = '<h2>{{ graph_1_d1_data }} kBT
 
 document.getElementById('graph_1_d2').innerHTML = '<h2>{{ graph_1_d2_data }} $</h2>';
 
-
-
-var graph_1_a2 = echarts.init(document.getElementById('graph_1_a2'));	
-window.onresize = function() {
-    graph_1_a2.resize();
-  };
-var option = {
-  xAxis: {
-    data: {{ graph_1_a2_names }}
-  },
-  yAxis: {
-	  name: 'dollar($)'
-  },
-  series: [
-    {
-      name: '销量',
-      type: 'bar',
-      data: {{ graph_1_a2_data }}
-	  label: {
-        normal: {
-          position: 'top',
-          distance: 10,
-          show: true,
-          formatter: ['Label Text'].join('\n'),
-          backgroundColor: '#eee',
-          borderColor: '#555',
-          borderWidth: 2,
-          borderRadius: 5,
-          padding: 10,
-          fontSize: 18,
-          shadowBlur: 3,
-          shadowColor: '#888',
-          shadowOffsetX: 0,
-          shadowOffsetY: 3,
-          textBorderColor: '#000',
-          textBorderWidth: 3,
-          color: '#992233'
-        }
-      }
-    }
-  ]
-};
-graph_1_a2.setOption(option);
+document.getElementById('graph_1_a2').innerHTML = '<h2>{{ graph_1_a2_data }} %</h2>';
 
 
 var graph_1_b1 = echarts.init(document.getElementById('graph_1_b1'));	
