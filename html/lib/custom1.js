3e70d3c3d3ee07b88793054a64607aa66d53202c
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-10-09 03:48:56 -0400

    unit opacity

--- html/lib/custom1.js
+++ html/lib/custom1.js
@@ -1,8 +1,8 @@
-document.getElementById('graph_1_d1').innerHTML = '<h1 class="mdui-center">{{ graph_1_d1_data }} kBTU</h1>';
+document.getElementById('graph_1_d1').innerHTML = '<h1 class="mdui-center">{{ graph_1_d1_data }}<span class="unit-opacity">kBTU</span></h1>';
 
-document.getElementById('graph_1_d2').innerHTML = '<h1 class="mdui-center">$ {{ graph_1_d2_data }}</h1>';
+document.getElementById('graph_1_d2').innerHTML = '<h1 class="mdui-center"><span class="unit-opacity">$</span>{{ graph_1_d2_data }}</h1>';
 
-document.getElementById('graph_1_a2').innerHTML = '<h1 class="mdui-center">{{ graph_1_a2_data }} %</h1>';
+document.getElementById('graph_1_a2').innerHTML = '<h1 class="mdui-center">{{ graph_1_a2_data }}<span class="unit-opacity">%</span></h1>';
 
 
 var graph_1_b1 = echarts.init(document.getElementById('graph_1_b1'));	
